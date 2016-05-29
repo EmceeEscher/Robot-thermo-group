fbda6e98d6818ec698fc691101db4c51d34f609a
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-05-28 22:28:31 -0700

    Fixed issues with 1d heat equation

--- src/simulations/heat_eq_1d.py
+++ src/simulations/heat_eq_1d.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 from math import pi
+from datetime import datetime
 import numpy as np
 
 # constants
@@ -19,22 +20,20 @@ TIME_STEP = 0.01
 X_ARRAY = np.linspace(MIN_X, MAX_X, DIM_X)
 
 
-# functions
-def _deriv_matrix(num_row, num_col, k):
-    m, n = num_row, num_col
-    mat = np.zeros(shape=(m, n))
-    for row in range(m):
+# data definitions
+def _deriv_matrix(size, k):
+    n = size
+    mat = np.eye(n)
+    for row in range(1, n-1):
         for col in range(n):
-            if (row, col) in [(0, 0), (m-1, n-1)]:
-                mat[row, col] = 1
-            elif abs(row - col) == 1:
+            if abs(row - col) == 1:
                 mat[row, col] = k
             elif row == col:
                 mat[row, col] = 1 - 2 * k
     return mat
 
 
-def explicit_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
+def _explicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     """Make a step forward in time using the explicit finite difference
     method for the heat equation:
         u_i^{n+1} = u_i^n + k * (u_{i+1}^n - 2*u_i^n + u_{i-1}^n),
@@ -53,7 +52,7 @@ def explicit_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
         elif idx_x == max_idx_x:
             u_1 = point_to_temp_map[idx_x-1]
             u1 = t_amb
-        else:
+        else:  # non-boundary
             u_1 = point_to_temp_map[idx_x-1]
             u1 = point_to_temp_map[idx_x+1]
         k = alpha * dt / dx**2
@@ -62,7 +61,7 @@ def explicit_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     return next_point_to_temp_map
 
 
-def implicit_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
+def _implicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     """Make a step forward in time using the implicit finite difference
     method for the heat equation:
         k*u_{i-1}^{n+1} + (1-2k)*u_i^{n+1} + k*u_{i+1}^{n+1} = u_i^n,
@@ -72,23 +71,24 @@ def implicit_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     max_idx_x = len(x_array) - 1
     dx = max_idx_x / (x_array[max_idx_x] - x_array[0])
     # make u^n vector
-    points = sorted(point_to_temp_map.values())
+    points = sorted(point_to_temp_map.keys())
     n = len(points)
-    u_prev = np.empty(shape=n+2)
+    u_prev = np.zeros(shape=n+2)
     for point, idx in zip(points, range(n)):
         u_prev[idx+1] = point_to_temp_map[point]
     # boundary condition ghost points
     u_prev[0] = t_src
     u_prev[n+1] = t_amb
     # make matrix
-    mat = _deriv_matrix(num_row=n, num_col=n+2, k=-alpha * dt / dx**2)
+    mat = _deriv_matrix(size=n+2, k=-alpha * dt / dx**2)
     # solve for u^{n+1} vector
     u_next = np.linalg.solve(mat, u_prev)
     # return point -> temp map
-    return {p: t for p, t in zip(points, u_next)}
+    return {p: t for p, t in zip(points, u_next[1:-1])}
 
 
-def crank_nicolson_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
+def _crank_nicolson_step_func(
+        point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     """Make a step forward in time using the Crank-Nicolson finite difference
     method for the heat equation:
          k*u_{i-1}^{n+1} + (1-2k)*u_i^{n+1} + k*u_{i+1}^{n+1} =
@@ -99,28 +99,47 @@ def crank_nicolson_step(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     max_idx_x = len(x_array) - 1
     dx = max_idx_x / (x_array[max_idx_x] - x_array[0])
     # make u^n vector
-    points = sorted(point_to_temp_map.values())
+    points = sorted(point_to_temp_map.keys())
     n = len(points)
-    u_prev = np.empty(shape=n+2)
+    u_prev = np.zeros(shape=n+2)
     for point, idx in zip(points, range(n)):
         u_prev[idx+1] = point_to_temp_map[point]
     # boundary condition ghost points
     u_prev[0] = t_src
     u_prev[n+1] = t_amb
-    # make matrix for u_n
+    # make matrix for u^n
     k1 = .5 * alpha * dt / dx**2
-    mat1 = _deriv_matrix(num_row=n+2, num_col=n+2, k=k1)
+    mat1 = _deriv_matrix(size=n+2, k=k1)
     u_prev = np.dot(mat1, u_prev)
+    # make matrix for u^{n+1}
     k2 = -k1
-    mat2 = _deriv_matrix(num_row=n, num_col=n+2, k=k2)
+    mat2 = _deriv_matrix(size=n+2, k=k2)
     # solve for u^{n+1} vector
     u_next = np.linalg.solve(mat2, u_prev)
     # return point -> temp map
-    return {p: t for p, t in zip(points, u_next)}
+    return {p: t for p, t in zip(points, u_next[1:-1])}
 
 
+class FiniteStepMethod:
+    def __init__(self, f, name):
+        self._f = f
+        self.name = name
+
+    def __call__(self, *args, **kwargs):
+        return self._f(*args, **kwargs)
+
+
+explicit = FiniteStepMethod(f=_explicit_step_func, name='Explicit')
+
+implicit = FiniteStepMethod(f=_implicit_step_func, name='Implicit')
+
+crank_nicolson = FiniteStepMethod(
+    f=_crank_nicolson_step_func, name='Crank-Nicolson')
+
+
+# functions
 def simulation(time_step, x_array, t_src, t_amb, t_0, alpha,
-               finite_step_method=crank_nicolson_step):
+               finite_step_method=crank_nicolson):
     # initialize points
     point_to_temp_map = {idx_x: t_0 for idx_x in range(len(x_array))}
     # begin finite differencing
@@ -134,12 +153,61 @@ def simulation(time_step, x_array, t_src, t_amb, t_0, alpha,
         yield point_to_temp_map
 
 
-# script
-s = simulation(
-    time_step=TIME_STEP, x_array=X_ARRAY, t_src=T_SRC, t_amb=T_AMB,
-    t_0=T_0, alpha=ALPHA, finite_step_method=explicit_step)
+def run_simulation(
+        fpath, verbose,
+        dim_x, min_x, max_x, alpha, t_0, t_amb, t_src,
+        num_steps, time_step, finite_step_method
+):
+    x_array = np.linspace(min_x, max_x, dim_x)
+    s = simulation(
+        time_step=time_step, x_array=x_array,
+        t_src=t_src, t_amb=t_amb, t_0=t_0, alpha=alpha,
+        finite_step_method=finite_step_method
+    )
+    lines = [
+        ' {}\n'.format(str(datetime.now())),
+        '\n',
+        ' Simulation of heat flow through 1-dimensional rod\n',
+        '\n',
+        ' Dimensions\n',
+        '   dim_x=        {}\n'.format(dim_x),
+        '   min_x=        {}\n'.format(min_x),
+        '   max_x=        {}\n'.format(max_x),
+        '\n',
+        ' Thermodynamical parameters\n',
+        '   alpha=        {}\n'.format(alpha),
+        '   t_0=          {}\n'.format(t_0),
+        '   t_amb=        {}\n'.format(t_amb),
+        '   t_src=        {}\n'.format(t_src),
+        '\n',
+        ' Finite differencing\n',
+        '   num_steps=    {}\n'.format(num_steps),
+        '   time_step=    {}\n'.format(time_step),
+        '   method=       {}\n'.format(finite_step_method.name),
+        '\n',
+    ]
+    if verbose:
+        print(''.join(lines), end='\n')
+    with open(fpath, 'w') as f:
+        for line in lines:
+            f.write('#' + line)
+        for point_to_temp_map, step in zip(s, range(num_steps+1)):
+            line = 'Step= {}\n'.format(step)
+            f.write(line)
+            if verbose:
+                print(line, end='')
+            for point, temp in point_to_temp_map.items():
+                line = '{:8}  {:16.8f}\n'.format(point, temp)
+                f.write(line)
+                if verbose:
+                    print(line, end='')
 
-for point_to_temp_map, step in zip(s, range(NUM_STEPS)):
-    for point, temp in sorted(point_to_temp_map.items()):
-        print('{:8.2f}'.format(temp), end=' ')
-    print()
+
+# script
+run_simulation(
+    fpath='./try.dat', verbose=True,
+    dim_x=DIM_X, min_x=MIN_X, max_x=MAX_X, alpha=ALPHA,
+    t_0=T_0, t_amb=T_AMB, t_src=T_SRC,
+    num_steps=NUM_STEPS, time_step=TIME_STEP,
+    finite_step_method=crank_nicolson
+)
