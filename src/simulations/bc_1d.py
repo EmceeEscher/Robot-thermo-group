ebed71128087b1a0e2fc825f3743f757696abd24
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-06-04 19:21:34 -0700

    Implemented abstracted method of specifying boundary conditions

---
+++ src/simulations/bc_1d.py
@@ -0,0 +1,126 @@
+from __future__ import division, print_function
+import numpy as np
+
+
+def _x0_d0(f_t):
+    def bc(d_mat, u_vect, t):
+        d_mat[:2] = np.zeros_like(d_mat[:2])
+        d_mat[0, 0] = 1
+        d_mat[1, 1] = 1
+        u_vect[0] = 1
+        u_vect[1] = f_t(t)
+        return d_mat, u_vect, t
+    return bc
+
+
+def _x0_d1(g_t, dx):
+    def bc(d_mat, u_vect, t):
+        d_mat[0] = np.zeros_like(d_mat[0])
+        d_mat[0, 0] = 1
+        u_vect[0] = u_vect[2] - 2 * dx * g_t(t)
+        return d_mat, u_vect, t
+    return bc
+
+
+def _x1_d0(f_t):
+    def bc(d_mat, u_vect, t):
+        d_mat[-2:] = np.zeros_like(d_mat[-2:])
+        d_mat[-2, -2] = 1
+        d_mat[-1, -1] = 1
+        u_vect[-2] = f_t(t)
+        u_vect[-1] = 1
+        return d_mat, u_vect, t
+    return bc
+
+
+def _x1_d1(g_t, dx):
+    def bc(d_mat, u_vect, t):
+        d_mat[-1] = np.zeros_like(d_mat[-1])
+        d_mat[-1, -1] = 1
+        u_vect[-1] = u_vect[-3] + 2 * dx * g_t(t)
+        return d_mat, u_vect, t
+    return bc
+
+
+class BoundaryConditions:
+    def __init__(self, x0_func, x1_func, x0_order=1, x1_order=1, name=None):
+        self._x0_func = x0_func
+        self._x1_func = x1_func
+        self.x0_order = x0_order
+        self.x1_order = x1_order
+        self.name = name
+
+    def __call__(self, *args, **kwargs):
+        return self._x1_func(*self._x0_func(*args, **kwargs))
+
+
+def get_bc_dirichlet(x0, x1):
+    return BoundaryConditions(
+        x0_func=_x0_d0(f_t=lambda t: x0),
+        x1_func=_x1_d0(f_t=lambda t: x1),
+        x0_order=1, x1_order=1,
+        name='Dirichlet',
+    )
+
+
+def get_bc_neumann(x0, x1, dx):
+    return BoundaryConditions(
+        x0_func=_x0_d1(g_t=lambda t: x0, dx=dx),
+        x1_func=_x1_d1(g_t=lambda t: x1, dx=dx),
+        x0_order=2, x1_order=2,
+        name='Neumann',
+    )
+
+
+def get_bc_mixed1(x0, x1, dx):
+    return BoundaryConditions(
+        x0_func=_x0_d0(f_t=lambda t: x0),
+        x1_func=_x1_d1(g_t=lambda t: x1, dx=dx),
+        x0_order=1, x1_order=2,
+        name='Mixed I',
+    )
+
+
+def get_bc_mixed2(x0, x1, dx):
+    return BoundaryConditions(
+        x0_func=_x0_d1(g_t=lambda t: x0, dx=dx),
+        x1_func=_x1_d0(f_t=lambda t: x1),
+        x0_order=2, x1_order=1,
+        name='Mixed II',
+    )
+
+
+def get_bc_time_dirichlet(x0_func, x1_func):
+    return BoundaryConditions(
+        x0_func=_x0_d0(f_t=x0_func),
+        x1_func=_x1_d0(f_t=x1_func),
+        x0_order=1, x1_order=1,
+        name='Time-dependent Dirichlet',
+    )
+
+
+def get_bc_time_neumann(x0_func, x1_func, dx):
+    return BoundaryConditions(
+        x0_func=_x0_d1(g_t=x0_func, dx=dx),
+        x1_func=_x1_d1(g_t=x1_func, dx=dx),
+        x0_order=2, x1_order=2,
+        name='Time-dependent Neumann',
+    )
+
+
+def get_bc_time_mixed1(x0_func, x1_func, dx):
+    return BoundaryConditions(
+        x0_func=_x0_d0(f_t=x0_func),
+        x1_func=_x1_d1(g_t=x1_func, dx=dx),
+        x0_order=1, x1_order=2,
+        name='Time-dependent Mixed I',
+    )
+
+
+def get_bc_time_mixed2(x0_func, x1_func, dx):
+    return BoundaryConditions(
+        x0_func=_x0_d1(g_t=x0_func, dx=dx),
+        x1_func=_x1_d0(f_t=x1_func),
+        x0_order=2, x1_order=1,
+        name='Time-dependent Mixed II',
+    )
--- src/simulations/heat_eq_1d.py
+++ src/simulations/heat_eq_1d.py
@@ -1,34 +1,24 @@
 from __future__ import division, print_function
 from datetime import datetime
-import numpy as np
+from bc_1d import *
+
 
 # constants
 # dimensions
 MIN_X = 0.0
 MAX_X = 1.0
-DIM_X = 10
+DIM_X = 11
 # thermodynamical parameters
 ALPHA = 1.0
 T_0 = 300.0  # initial temperature
 T_AMB = 300.0  # ambient temperature
 T_SRC = 1000.0  # heating temperature
 # finite differencing
-NUM_STEPS = 1000
+NUM_STEPS = 100000
 TIME_STEP = 0.01
+WRITE_PERIOD = 100
 # points
 X_ARRAY = np.linspace(MIN_X, MAX_X, DIM_X)
-
-
-def _deriv_matrix(size, k):
-    n = size
-    mat = np.eye(n)
-    for row in range(1, n-1):
-        for col in range(n):
-            if abs(row - col) == 1:
-                mat[row, col] = k
-            elif row == col:
-                mat[row, col] = 1 - 2 * k
-    return mat
 
 
 def _d1_matrix(n, diff=1):
@@ -48,45 +38,15 @@ def _d2_matrix(n, diff=1):
     return np.dot(_d1_matrix(n=n-diff, diff=diff), _d1_matrix(n=n, diff=diff))
 
 
-# boundary conditions
-def _x0_d0(f_t):
-    def bc(d_mat, u_vect, t):
-        d_mat[0] = np.zeros_like(d_mat[0])
-        d_mat[0, 0] = 1
-        u_vect[0] = f_t(t)
-        return d_mat, u_vect, t
-    return bc
-
-
-def _x0_d1(g_t):
-    def bc(d_mat, u_vect, t, dx):
-        d_mat[0] = np.zeros_like(d_mat[0])
-        d_mat[0, 0] = 1
-        u_vect[0] = u_vect[2] - 2 * dx * g_t(t)
-        return d_mat, u_vect, t
-    return bc
-
-
-def _x1_d0(f_t):
-    def bc(d_mat, u_vect, t):
-        d_mat[-1] = np.zeros_like(d_mat[-1])
-        d_mat[-1, -1] = 1
-        u_vect[-1] = f_t(t)
-        return d_mat, u_vect, t
-    return bc
-
-
-def _x1_d1(g_t):
-    def bc(d_mat, u_vect, t, dx):
-        d_mat[-1] = np.zeros_like(d_mat[-1])
-        d_mat[-1, -1] = 1
-        u_vect[-1] = u_vect[-3] + 2 * dx * g_t(t)
-        return d_mat, u_vect, t
-    return bc
+def _heat_eq_matrix(n, k):
+    zerow = np.zeros(shape=(1, n+2))
+    return np.eye(n+2) + k * np.r_[zerow, _d2_matrix(n+2), zerow]
 
 
 # step functions
-def _explicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
+def _explicit_step_func(
+        point_to_temp_map, time, x_array, dt, alpha, boundary_conditions,
+):
     """Make a step forward in time using the explicit finite difference
     method for the heat equation:
         u_i^{n+1} = u_i^n + k * (u_{i+1}^n - 2*u_i^n + u_{i-1}^n),
@@ -101,18 +61,19 @@ def _explicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     u_prev = np.zeros(shape=n+2)
     for point, idx in zip(points, range(n)):
         u_prev[idx+1] = point_to_temp_map[point]
-    # boundary condition ghost points
-    u_prev[0] = t_src
-    u_prev[n+1] = t_amb
     # make matrix
-    mat = _deriv_matrix(size=n+2, k=alpha*dt/dx**2)
+    mat = _heat_eq_matrix(n=n, k=alpha*dt/dx**2)
+    # implement boundary conditions
+    mat, u_prev, time = boundary_conditions(d_mat=mat, u_vect=u_prev, t=time)
     # get u^{n+1} vector
     u_next = np.dot(mat, u_prev)
     # return point -> temp map
     return {p: t for p, t in zip(points, u_next[1:-1])}
 
 
-def _implicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
+def _implicit_step_func(
+        point_to_temp_map, time, x_array, dt, alpha, boundary_conditions,
+):
     """Make a step forward in time using the implicit finite difference
     method for the heat equation:
         k*u_{i-1}^{n+1} + (1-2k)*u_i^{n+1} + k*u_{i+1}^{n+1} = u_i^n,
@@ -127,11 +88,10 @@ def _implicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
     u_prev = np.zeros(shape=n+2)
     for point, idx in zip(points, range(n)):
         u_prev[idx+1] = point_to_temp_map[point]
-    # boundary condition ghost points
-    u_prev[0] = t_src
-    u_prev[n+1] = t_amb
     # make matrix
-    mat = _deriv_matrix(size=n+2, k=-alpha * dt / dx**2)
+    mat = _heat_eq_matrix(n=n, k=-alpha * dt / dx**2)
+    # implement boundary conditions
+    mat, u_prev, time = boundary_conditions(d_mat=mat, u_vect=u_prev, t=time)
     # solve for u^{n+1} vector
     u_next = np.linalg.solve(mat, u_prev)
     # return point -> temp map
@@ -139,7 +99,8 @@ def _implicit_step_func(point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
 
 
 def _crank_nicolson_step_func(
-        point_to_temp_map, x_array, dt, alpha, t_src, t_amb):
+        point_to_temp_map, time, x_array, dt, alpha, boundary_conditions,
+):
     """Make a step forward in time using the Crank-Nicolson finite difference
     method for the heat equation:
          k*u_{i-1}^{n+1} + (1-2k)*u_i^{n+1} + k*u_{i+1}^{n+1} =
@@ -155,15 +116,14 @@ def _crank_nicolson_step_func(
     u_prev = np.zeros(shape=n+2)
     for point, idx in zip(points, range(n)):
         u_prev[idx+1] = point_to_temp_map[point]
-    # boundary condition ghost points
-    u_prev[0] = t_src
-    u_prev[n+1] = t_amb
     # make matrix for u^n
     k1 = .5 * alpha * dt / dx**2
-    mat1 = _deriv_matrix(size=n+2, k=k1)
+    mat1 = _heat_eq_matrix(n=n, k=k1)
     # make matrix for u^{n+1}
     k2 = -k1
-    mat2 = _deriv_matrix(size=n+2, k=k2)
+    mat2 = _heat_eq_matrix(n=n, k=k2)
+    # implement boundary conditions
+    mat, u_prev, time = boundary_conditions(d_mat=mat2, u_vect=u_prev, t=time)
     # solve for u^{n+1} vector
     u_next = np.linalg.solve(mat2, np.dot(mat1, u_prev))
     # return point -> temp map
@@ -188,31 +148,35 @@ crank_nicolson = FiniteStepMethod(
 
 
 # functions
-def simulation(time_step, x_array, t_src, t_amb, t_0, alpha,
-               finite_step_method=crank_nicolson):
+def simulation(
+        time_step, x_array, t_0, alpha, finite_step_method,
+        boundary_conditions,
+):
     # initialize points
     point_to_temp_map = {idx_x: t_0 for idx_x in range(len(x_array))}
+    time = 0
     # begin finite differencing
     yield point_to_temp_map
     while True:
         point_to_temp_map = finite_step_method(
             point_to_temp_map=point_to_temp_map,
-            x_array=x_array, dt=time_step,
-            alpha=alpha, t_src=t_src, t_amb=t_amb
+            time=time, x_array=x_array, dt=time_step,
+            alpha=alpha, boundary_conditions=boundary_conditions,
         )
+        time += time_step
         yield point_to_temp_map
 
 
 def run_simulation(
-        fpath, verbose,
-        dim_x, min_x, max_x, alpha, t_0, t_amb, t_src,
-        num_steps, time_step, finite_step_method
+        fpath, verbose, write_period,
+        dim_x, min_x, max_x, alpha, t_0,
+        num_steps, time_step, finite_step_method, boundary_conditions,
 ):
     x_array = np.linspace(min_x, max_x, dim_x)
     s = simulation(
         time_step=time_step, x_array=x_array,
-        t_src=t_src, t_amb=t_amb, t_0=t_0, alpha=alpha,
-        finite_step_method=finite_step_method
+        t_0=t_0, alpha=alpha, finite_step_method=finite_step_method,
+        boundary_conditions=boundary_conditions,
     )
     lines = [
         ' {}\n'.format(str(datetime.now())),
@@ -227,8 +191,11 @@ def run_simulation(
         ' Thermodynamical parameters\n',
         '   alpha=        {}\n'.format(alpha),
         '   t_0=          {}\n'.format(t_0),
-        '   t_amb=        {}\n'.format(t_amb),
-        '   t_src=        {}\n'.format(t_src),
+        '\n',
+        ' Boundary conditions:\n',
+        '   method=       {}\n'.format(boundary_conditions.name),
+        '   x0_order=     {}\n'.format(boundary_conditions.x0_order),
+        '   x1_order=     {}\n'.format(boundary_conditions.x1_order),
         '\n',
         ' Finite differencing\n',
         '   num_steps=    {}\n'.format(num_steps),
@@ -242,23 +209,24 @@ def run_simulation(
         for line in lines:
             f.write('#' + line)
         for point_to_temp_map, step in zip(s, range(num_steps+1)):
-            line = 'Step= {}\n'.format(step)
-            f.write(line)
-            if verbose:
-                print(line, end='')
-            for point, temp in point_to_temp_map.items():
-                line = '{:8}  {:16.8f}\n'.format(point, temp)
+            if step % write_period == 0:
+                line = 'Step= {}\n'.format(step)
                 f.write(line)
                 if verbose:
                     print(line, end='')
+                for point, temp in point_to_temp_map.items():
+                    line = '{:8}  {:16.8f}\n'.format(point, temp)
+                    f.write(line)
+                    if verbose:
+                        print(line, end='')
 
 
 # script
 if __name__ == '__main__':
     run_simulation(
-        fpath='./try.dat', verbose=True,
+        fpath='./try.dat', verbose=True, write_period=WRITE_PERIOD,
         dim_x=DIM_X, min_x=MIN_X, max_x=MAX_X, alpha=ALPHA,
-        t_0=T_0, t_amb=T_AMB, t_src=T_SRC,
-        num_steps=NUM_STEPS, time_step=TIME_STEP,
-        finite_step_method=crank_nicolson
+        t_0=T_0, num_steps=NUM_STEPS, time_step=TIME_STEP,
+        finite_step_method=implicit,
+        boundary_conditions=get_bc_dirichlet(x0=T_SRC, x1=T_AMB)
     )
