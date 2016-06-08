f618fba1e9c0e561622d0314e6df639ee3d47b69
Ryan <rywais@gmail.com>
2016-06-08 16:08:51 -0700

    Fixed the automated data processing function

--- src/convertToTemp.m
+++ src/convertToTemp.m
@@ -5,7 +5,7 @@ p = polyfit(T_cal,V_cal,1);
 T_dat = (V_dat - p(2))/p(1);
 
 f = fopen(outFile,'w');
-for i = 1:size(T_dat)
+for i = 1:size(T_dat')
    fprintf(f, '%16.8f %16.8f\n\r', t(i),T_dat(i)); 
 end
 fclose(f);
--- src/quickwrite.m
+++ src/quickwrite.m
@@ -2,11 +2,15 @@ function quickwrite(vFile,calFile,smthVFile,tFile)
 
 %Enter all files as the path STARTING from in the data folder
 
-[x,y] = get_calibration(['../data/' vFile]);
+[xp,yp] = get_calibration(['../data/' vFile]);
 
-averageValues(x,y,['../data/' smthVFile]);
+averageValues(xp,yp,['../data/' smthVFile]);
+
+% [time,temp] = convertToTemp( ...
+%     ['../data/' smthVFile], ['../data/' calFile], ['../data/' tFile]);
 
 [time,temp] = convertToTemp( ...
-    ['../data/' smthVFile], ['../data/' calFile], ['../data/' tFile])
+    ['../data/' smthVFile], ['../data/calibration stuff/calibrate_tc1_May_25_Trial_3.dat'], ...
+    ['../data/temperature data/June 1/FUCK']);
 
 end
