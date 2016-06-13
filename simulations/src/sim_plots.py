96cd954627f47b2c911bc338dc9006be397b69a0
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-06-12 22:11:46 -0700

    plot for simulations, still more to come

--- simulations/src/sim_plots.py
+++ simulations/src/sim_plots.py
@@ -1,3 +1,4 @@
+import re
 from os import path
 from matplotlib import pyplot as plt
 from matplotlib import cm
@@ -9,6 +10,8 @@ FPATHS_LIST = [
     '../../data/temperature data/June 8/Run1_tc{}.dat'.format(i)
     for i in range(1, 5)
 ]
+
+SIM_FPATH = '../results/June 6 - Run 2/opt_test-sim.dat'
 
 
 def plot_experimental_data(fpaths_list):
@@ -56,6 +59,8 @@ def plot_experimental_data_surface(fpaths_list):
         else:
             time_list.append(ldats[0][0])
             temp_arr_list.append(np.array([ldat[1]+273.15 for ldat in ldats]))
+    for f in files:
+        f.close()
     x_arr = np.arange(1, len(fpaths_list)+1)
     time_arr = np.array(time_list)[::2]
     temp_arr = np.array(temp_arr_list)[::2]
@@ -71,12 +76,69 @@ def plot_experimental_data_surface(fpaths_list):
     return surf
 
 
+def _get_simulation_data(fpath):
+    steps = list()
+    temp_list = list()
+    with open(fpath, 'r') as fr:
+        while True:
+            line = fr.readline()
+            if re.match('\s*[0-9]+\s+[0-9]+\.', line):
+                temp_list.append(float(line.split()[1]))
+            elif len(temp_list) > 0:
+                steps.append(np.array(temp_list))
+                temp_list = list()
+                if not line:
+                    break
+    return np.array(steps)
+
+
+def _get_simulation_params_dict(fpath):
+    params = dict()
+    with open(fpath, 'r') as fr:
+        for line in fr:
+            if re.match('#.*=\s+[0-9]+\.', line):
+                name, param = [s.strip('#').strip() for s in line.split('=')]
+                params[name] = float(param)
+            elif re.match('#.*=\s+[0-9]', line):
+                name, param = line.split('=')
+                params[name] = int(param)
+            elif re.match('#.*=\s+', line):
+                name, param = line.split('=')
+                params[name] = param
+            elif re.match('Step', line):
+                break
+    return params
+
+
+def plot_simulation_data(fpath):
+    # get data
+    temp_array = _get_simulation_data(fpath)
+    time_step = _get_simulation_params_dict(fpath)['time step']
+    time_array = time_step * np.arange(len(temp_array))
+    plots = list()
+    for tc_dat, i in zip(temp_array.T, range(len(temp_array.T))):
+        plots.append((time_array, tc_dat, 'Thermocouple {}'.format(i)))
+    # make figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    lines = list()
+    for x, y, lab in plots:
+        lines.append(ax.plot(x, y, label=lab))
+    plt.xlabel('Time (s)')
+    plt.ylabel('Temperature (K)')
+    plt.title('Temperature vs Time Data for Simulation')
+    return lines
+
 
 # script
 if __name__ == '__main__':
-    plot_experimental_data(fpaths_list=FPATHS_LIST)
-    plt.legend()
-    plt.show()
+    # plot_experimental_data(fpaths_list=FPATHS_LIST)
+    # plt.legend()
+    # plt.show()
+    #
+    # plot_experimental_data_surface(fpaths_list=FPATHS_LIST)
+    # plt.show()
 
-    plot_experimental_data_surface(fpaths_list=FPATHS_LIST)
+    plot_simulation_data(fpath=SIM_FPATH)
+    plt.legend()
     plt.show()
