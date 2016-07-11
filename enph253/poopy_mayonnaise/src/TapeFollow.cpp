681921c5082923c46c5e6adabdddce2f2375cbb2
Jacob Brunette <jacob.brunette@ubcsolar.com>
2016-07-11 11:48:16 -0700

    same error as dilyn fixed

--- enph253/poopy_mayonnaise/src/TapeFollow.cpp
+++ enph253/poopy_mayonnaise/src/TapeFollow.cpp
@@ -135,7 +135,7 @@ void TapeFollow::loop() {
 
     
     // get net effect of proportional and derivative gains
-    prop = (propGain * this->error);
+    prop = (propGain * error);
     derv = (static_cast<float>(dervGain) *
 	    static_cast<float>(error - this->recentError)
 	    / static_cast<float>(this->prevTime - this->timeStep));
