3a2cdae2a7eb4a94749b02428ec671a7469902be
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-07-18 10:37:56 -0700

    Use calculated second derivative gain

--- enph253/poopy_mayonnaise/src/TapeFollow3.cpp
+++ enph253/poopy_mayonnaise/src/TapeFollow3.cpp
@@ -276,7 +276,7 @@ void TapeFollow3::loop()
     // TODO move this to constructor once values are decided upon
     this->gainProp = GAIN_PROP;//static_cast<double>(knob(KNOB_PROP_GAIN)) / 50.;
     this->gainDer1 = GAIN_DER1;//static_cast<double>(knob(KNOB_DER1_GAIN)) / 50.;
-    this->gainDer2 = 0.; //.5*this->gainDer1*this->gainDer1/this->gainProp*(1.-EPSILON);
+    this->gainDer2 = .5*this->gainDer1*this->gainDer1/this->gainProp*(1.-EPSILON);
 
     // get readings from tape sensors
     for (auto i(0); i < 4; ++i) {
