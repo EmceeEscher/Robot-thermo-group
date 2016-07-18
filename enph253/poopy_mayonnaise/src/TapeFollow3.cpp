0efcb4283eb14efe556ea11bad2f07ad8a44fb72
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-07-18 10:41:12 -0700

    Set gains based on knobs

--- enph253/poopy_mayonnaise/src/TapeFollow3.cpp
+++ enph253/poopy_mayonnaise/src/TapeFollow3.cpp
@@ -274,8 +274,8 @@ void TapeFollow3::loop()
 
     // set gains
     // TODO move this to constructor once values are decided upon
-    this->gainProp = GAIN_PROP;//static_cast<double>(knob(KNOB_PROP_GAIN)) / 50.;
-    this->gainDer1 = GAIN_DER1;//static_cast<double>(knob(KNOB_DER1_GAIN)) / 50.;
+    this->gainProp = static_cast<double>(knob(KNOB_PROP_GAIN)) / 50.;
+    this->gainDer1 = static_cast<double>(knob(KNOB_DER1_GAIN)) / 50.;
     this->gainDer2 = .5*this->gainDer1*this->gainDer1/this->gainProp*(1.-EPSILON);
 
     // get readings from tape sensors
