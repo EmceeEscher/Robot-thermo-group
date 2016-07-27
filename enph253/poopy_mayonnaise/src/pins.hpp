f880e435792ffc927de80418dfd017298eac92bd
Jacob Brunette <jacob.brunette@ubcsolar.com>
2016-07-27 13:38:05 -0700

    integrating ArmControl with pins file

--- enph253/poopy_mayonnaise/src/pins.hpp
+++ enph253/poopy_mayonnaise/src/pins.hpp
@@ -15,15 +15,24 @@ namespace pins
     // Digital in
     const int TAPE_SENSORS_FRONT[] { 0,  1,  2,  3};  // << < > >>
     const int TAPE_SENSORS_BACK[]  { 8,  9, 10, 11};  // << < > >>
-    const int COLLISION_SENSORS[]  { 5,  4,  6,  7};  // left front right back
+    const int COLLISION_SENSORS[]  { 4, 5 };  // front, back
+    const int ARM_SWITCHES[] {12, 13, 14}; //catch, miss, inner
+    const int PASSENGER_SENSOR_BACK_LEFT {6}; //we ran out of analog pins, so this one has to be digital
     
     // Analog in
-    const int PASSENGER_SENSORS_SIDES[] {0, 1, 2, 3, 4, 5};
-	const int BEACON_SENSORS_SIDES[] {6,7}; // left right (Requires loss of knobs)
+    const int PASSENGER_SENSORS_SIDES[] {0, 1, 2, 3, 4};
+	  const int BEACON_SENSORS_SIDES[] {6,7}; // left right (Requires loss of knobs)
+    const int POTENTIOMETER {5};
 
     // Digital out
     const int MOTOR_PIN_L {0};
+    const int MOTOR_PIN_ARM {1};
+    const int MOTOR_PIN_BABY {2};
     const int MOTOR_PIN_R {3};
+
+    //Servo motors used as digital outputs for stepper motor
+    const int PULSE_PIN {31};
+    const int DIR_PIN {34};
 
     // Analog out
 
