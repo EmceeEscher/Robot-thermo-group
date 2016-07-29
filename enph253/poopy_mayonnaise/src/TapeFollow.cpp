f20b30504759b422c8b74b10dcd2a89ffb1568f0
Jacob Brunette <jacob.brunette@ubcsolar.com>
2016-07-29 14:15:44 -0700

    fixed previous fix

--- enph253/poopy_mayonnaise/src/TapeFollow.cpp
+++ enph253/poopy_mayonnaise/src/TapeFollow.cpp
@@ -259,9 +259,9 @@ Direction TapeFollow::chooseTurn(bool left, bool right, bool straight)
     float rightProb;
     float straightProb;
     if(abs(total)<0.0001){
-      leftProb = left * this->leftWeight / (left+right+straight);
-      rightProb = right * this->rightWeight / (left+right+straight);
-      straightProb = straight * this->straightWeight / (left+right+straight);
+      leftProb = left / (left+right+straight);
+      rightProb = right / (left+right+straight);
+      straightProb = straight / (left+right+straight);
     }else{
       leftProb     = left     * this->leftWeight     / total;
       rightProb    = right    * this->rightWeight    / total;
