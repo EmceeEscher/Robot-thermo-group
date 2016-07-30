e4e1dd8887fb72b654a804e229b9d24ccd9f6049
Jacob Brunette <jacob.brunette@ubcsolar.com>
2016-07-30 16:01:30 -0700

    pins

--- enph253/robot - ralph version/main/TapeFollow.ino
+++ enph253/robot - ralph version/main/TapeFollow.ino
@@ -56,12 +56,12 @@ int motorSpeed;               // speed to add to motors
 unsigned long tapeFollowSteps;
 double lastError;             // last calculated error
 
-vector<bool> intersectSeen;        // true if an intersection was seen
-vector<bool> intersectDetect;      // true when an intersection has been detected (seen and passed over)
+bool intersectSeen [2];        // true if an intersection was seen
+bool intersectDetect [2];      // true when an intersection has been detected (seen and passed over)
 
-vector<double> errorArray;         // array of last 2 distinct errors
-vector<unsigned long> etimeArray;  // array of times (since read) assoc with errorArray
-vector<int> activePins;
+double errorArray [2];         // array of last 2 distinct errors
+unsigned long etimeArray [2];   // array of times (since read) assoc with errorArray
+int activePins [4];
 
 void TapeFollow3::init()
 {
